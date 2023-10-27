013810d0c1add8646e4512bc63677b9b8316fca6
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-10-26 21:56:26 -0500

    Update contar_palabras.py

--- contar_palabras.py
+++ contar_palabras.py
@@ -1,2 +1,6 @@
 import re
 from collections import defaultdict
+def contar_palabras_en_archivo(archivo):
+    """
+    Esta función toma un archivo de texto como entrada y cuenta cuántas veces aparece cada palabra en el archivo.
+    """
